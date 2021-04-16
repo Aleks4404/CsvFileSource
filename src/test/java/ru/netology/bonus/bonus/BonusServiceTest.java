11c16b0682120fa5206583f11e6e2506d61d9a3e
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-16 05:46:59 +0400

    update CSV

--- src/test/java/ru/netology/bonus/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/bonus/BonusServiceTest.java
@@ -1,14 +1,14 @@
 package ru.netology.bonus.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
     @ParameterizedTest
-    @CsvSource( "/data.csv")
-     void shouldCalculate(String test, long amount, boolean registered, long expected) {
+    @CsvFileSource(resources = "/data.csv")
+    void shouldCalculate(String test, long amount, boolean registered, long expected) {
 
         ru.netology.bonus.BonusService service = new ru.netology.bonus.BonusService();
 
