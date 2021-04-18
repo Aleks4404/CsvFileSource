a2bb9e8a83f40c4958be3d48de4819ed9ec0c63d
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-18 13:10:20 +0400

    update BonusServiceTest 1.1

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -17,7 +17,7 @@ class BonusServiceTest {
 
             }
     )
-    @CsvFileSource(resources = "/data.csv", delimiter = '/')
+    @CsvFileSource(resources = "/data.csv")
     void shouldCalculate(String testName, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
 
